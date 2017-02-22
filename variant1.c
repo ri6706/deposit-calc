9d928a3b4f30202b442a7ca98ed385120e91bb53
Киселёва Диана Дмитриевна <ri67s06@mwp02.csc.local>
2017-02-22 08:46:37 +0700

    Add }

--- variant1.c
+++ variant1.c
@@ -8,11 +8,11 @@ int main()
     scanf("%d %d", &sum, &date);
 	
 	 if (date<=365 && sum>=10000 && sum<=100000){
-        if (date>0 && date=30){
-        sum = sum-sum*0.10;
+        if (date>0 && date<=30){
+        sum = sum - sum * 0.10;
         }
         if (date>30 && date<=120){
-        sum = sum+sum*0.02;
+        sum = sum + sum * 0.02;
         }
         if (date>120 && date<=240){
         sum = sum+sum*0.06;
@@ -20,12 +20,13 @@ int main()
         if (date>240 && date<=365){
         sum = sum+sum*0.12;
         }
+	}
     else{
          printf("Error");
          }
          
-        if (date<=365 && sum>=100000){
-        if (date>0 && date=30){
+      if (date<=365 && sum>=100000){
+        if (date>0 && date<=30){
         sum = sum-sum*0.10;
         }
         if (date>30 && date<=120){
@@ -37,6 +38,7 @@ int main()
         if (date>240 && date<=365){
         sum = sum+sum*0.15;
         }
+      }
     else{
          printf("Error");
          }
